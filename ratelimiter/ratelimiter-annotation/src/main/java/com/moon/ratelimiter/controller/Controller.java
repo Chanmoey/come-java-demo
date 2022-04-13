a3312dba776c216e7c6b71264196c5e45e456eea
Chanmoey <relijifa@gmail.com>
2022-04-13 15:57:13 +0800

    测试用的controller。

---
+++ ratelimiter/ratelimiter-annotation/src/main/java/com/moon/ratelimiter/controller/Controller.java
@@ -0,0 +1,21 @@
+package com.moon.ratelimiter.controller;
+
+import com.moon.ratelimiter.annotation.Limiter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Chanmoey
+ * @date 2022年04月13日
+ */
+@RestController
+@RequestMapping("/limiter")
+public class Controller {
+
+    @Limiter(limit = 1, key = "test")
+    @GetMapping("/test")
+    public String limit() {
+        return "success";
+    }
+}
