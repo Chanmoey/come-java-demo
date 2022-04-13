ade371b45a21add0e797a4d7ed525ff005cf171d
Chanmoey <relijifa@gmail.com>
2022-04-13 15:58:04 +0800

    AOP，对限流注解@Limiter的具体实现。

---
+++ ratelimiter/ratelimiter-annotation/src/main/java/com/moon/ratelimiter/LimiterApplication.java
@@ -0,0 +1,16 @@
+package com.moon.ratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Chanmoey
+ * @date 2022年04月12日
+ */
+@SpringBootApplication
+public class LimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimiterApplication.class, args);
+    }
+}
