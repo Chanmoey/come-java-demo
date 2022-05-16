e4b0eec927839588be4c049076d89afdff9e2ece
Chanmoey <relijifa@gmail.com>
2022-05-16 20:32:23 +0800

    异常类。

---
+++ renamefile/renamefile/src/main/java/com/moon/renamefile/exception/ChangeNameFailException.java
@@ -0,0 +1,11 @@
+package com.moon.renamefile.exception;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月16日
+ */
+public class ChangeNameFailException extends RuntimeException{
+    public ChangeNameFailException(String message) {
+        super(message);
+    }
+}
